5c55acab7b8440508b3199ae3f336774f82accf7
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-04 03:28:39 +0500

    fonts and images

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 const theme = {
   ...DefaultTheme,
@@ -14,6 +16,17 @@ const theme = {
 const Stack = createStackNavigator();
 
 const App = () => {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
