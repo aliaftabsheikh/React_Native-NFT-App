10b38415437861cb18afbe09b655557887d24d64
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-05 02:13:49 +0500

    Nft Card

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useNavigation } from "@react-navigation/native";
+import { View, Image, Text } from "react-native";
 
-const NFTcard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+
+const NFTcard = ({ data }) => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>NFTcard</Text>
-    </View>
-  )
-}
+    <View
+    style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark,
+    }}
+  >
+       <View
+        style={{
+          width: "100%",
+          height: 250,
+        }}
+      >
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
 
-export default NFTcard
+      </View>
+      <Text>Nft CArd</Text>
+      </View>
+  )
+};
+
+export default NFTcard;
