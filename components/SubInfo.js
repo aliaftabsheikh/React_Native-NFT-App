7220cf6fbc5f9e72933e45c3aa04dd54a4bec245
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-05 02:29:21 +0500

    nft card info structure

---
+++ components/SubInfo.js
@@ -0,0 +1,50 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export const NFTTitle = () => {
+  return (
+    <View>
+      <Text>SubInfo</Text>
+    </View>
+  );
+};
+
+export const EthPrice = () => {
+  return (
+    <View>
+      <Text>EthPrice</Text>
+    </View>
+  );
+};
+
+export const ImageCmp = () => {
+  return (
+    <View>
+      <Text>ImageCmp</Text>
+    </View>
+  );
+};
+
+export const People = () => {
+  return (
+    <View>
+      <Text>People</Text>
+    </View>
+  );
+};
+
+export const EndDate = () => {
+  return (
+    <View>
+      <Text>EndDate</Text>
+    </View>
+  );
+};
+
+export const Subinfo = () => {
+  return (
+    <View>
+      <Text>Subinfo</Text>
+    </View>
+  );
+};
