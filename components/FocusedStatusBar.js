01e24fd47e7dc6ae327d5828aae9487fbc5f5d37
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-05 02:05:22 +0500

    nft card

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,12 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from "react-native";
+import { useIsFocused } from "@react-navigation/core";
 
-const FocusStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusStatusBar</Text>
-    </View>
-  )
-}
+const FocusStatusBar = (props) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
+};
 
-export default FocusStatusBar
+export default FocusStatusBar;
