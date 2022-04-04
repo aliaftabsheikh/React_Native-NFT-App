11298f37d6022c63aee44fa28927b6b795907905
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-05 02:25:40 +0500

    Nft Card

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -2,20 +2,21 @@ import { useNavigation } from "@react-navigation/native";
 import { View, Image, Text } from "react-native";
 
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { RectButton, CircleButton } from "./Button";
 
 const NFTcard = ({ data }) => {
   const navigation = useNavigation();
   return (
     <View
-    style={{
-      backgroundColor: COLORS.white,
-      borderRadius: SIZES.font,
-      marginBottom: SIZES.extraLarge,
-      margin: SIZES.base,
-      ...SHADOWS.dark,
-    }}
-  >
-       <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View
         style={{
           width: "100%",
           height: 250,
@@ -31,11 +32,10 @@ const NFTcard = ({ data }) => {
             borderTopRightRadius: SIZES.font,
           }}
         />
-
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>Nft CArd</Text>
-      </View>
-  )
+    </View>
+  );
 };
 
 export default NFTcard;
--- components/index.js
+++ components/index.js
@@ -1,10 +1,13 @@
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
 
 
 export {
     HomeHeader,
     NFTCard,  
     FocusedStatusBar,
+    CircleButton,
+    RectButton
   };
