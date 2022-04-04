579fa6ec8176189783d2d4642b175d1325f2c483
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-04-05 01:39:06 +0500

    folder structure for home

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FocusStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusStatusBar</Text>
+    </View>
+  )
+}
+
+export default FocusStatusBar
--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
-import { View, SafeAreaView, FlatList } from "react-native";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
 import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+
+        </SafeAreaView>
+  )
 };
 
 export default Home;
