2c094492f735d15f5afea943733fe84cd598a642
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-05-11 12:23:20 +0500

    Style Details Page

--- screens/Details.js
+++ screens/Details.js
@@ -20,14 +20,28 @@ import {
 const DetailsHeader = ({ data, navigation }) => {
   return (
     <View style={{ width: "100%", height: 373 }}>
-      <Image source={data.image} />
+      <Image source={data.image}
+      resizeMode="cover"
+      style={{width : '100%', height: '100%'}}
+      />
+
+      <CircleButton imgUrl={assets.left} 
+      handlePress={()=> navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+      />
+
+      <CircleButton imgUrl={assets.heart} 
+      right={15}
+      top={StatusBar.currentHeight + 10}
+      />
+
     </View>
   );
 };
 
 const Details = ({ route, navigation }) => {
   const { data } = route.params;
-  console.log(data);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
