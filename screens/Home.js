482da6347408d05ac0f03d838f2abcc6ac862b26
ali-kodexo <aliaftabsheikh1@gmail.com>
2022-08-25 09:56:06 +0500

    Implement Search functionality

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,20 @@ import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value)=>{
+    if(!value.length) return setNftData(NFTData);
+
+
+    const filteredData = NFTData.filter((item)=> item.name.toLowerCase().includes(value.toLowerCase()));
+
+    if(filteredData){
+      setNftData(filteredData )
+    }else{
+      setNftData(NFTData)
+    }
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
@@ -11,11 +25,11 @@ const Home = () => {
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData} 
+            data={nftData} 
             renderItem={({ item }) => <NFTCard data={item}/>}
             keyExtractor={(item) => item.id} 
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>}
           />
         </View>
         <View
